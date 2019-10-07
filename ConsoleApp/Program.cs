8587fe5b8e19e1c5d78b6ab9188c2010e1a57c84
Robert Tõnisson <rotoni@ttu.ee>
2019-10-07 18:57:09 +0300

    ok

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace icd0008_2019f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
